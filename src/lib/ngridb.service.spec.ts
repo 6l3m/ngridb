553c01326f6bb9a090d2cb8196456f432a03b3ce
6l3m <clmpoulain@gmail.com>
2020-04-26 22:25:31 +0200

    wip ngridb service test

--- src/lib/ngridb.service.spec.ts
+++ src/lib/ngridb.service.spec.ts
@@ -1,20 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NgridbService } from './ngridb.service';
+import { DbService } from './db.service';
 
 describe('NgridbService', () => {
   let service: NgridbService<{ a: number; b: string }>;
+  let dbService: DbService;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(NgridbService);
+    dbService = TestBed.inject(DbService);
+    try {
+      await dbService.openDb('test', 1, ['test']);
+    } catch (error) {
+      console.error(error);
+    }
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('#add should add to database and store', () => {
-    service.add('test', '1');
+  it('db should not be undefined', () => {
+    expect(dbService.db).not.toBeUndefined();
+  });
+
+  xit('#add should add to database and store', () => {
+    service.add<'b'>('test', '1');
+  });
+
+  afterAll(() => {
+    indexedDB.deleteDatabase('test');
   });
 });
